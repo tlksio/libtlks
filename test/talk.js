b0bbb9c1ade1c60aa0d18661c0bcb64b6e16dbaf
Raül Pérez <repejota@gmail.com>
2015-02-18 11:36:43 +0100

    .

--- test/talk.js
+++ test/talk.js
@@ -1,18 +1,62 @@
 var assert = require("assert");
 var tlks = require("../index.js");
 
+var dburl = "mongodb://dbuser:dbpass@ds043180.mongolab.com:43180/techtalks";
+
 describe('Talk', function () {
 
-    it('get the latest talks', function () {
-        assert.equal(tlks.talk.latest(), true);
+    it('get the latest talks', function (done) {
+        tlks.talk.latest(dburl, 5, function (err, docs) {
+            if (err) {
+                throw new Error(err);
+            }
+            assert.notEqual(docs, null);
+            done();
+        });
     });
 
-    it('get the most popular talks', function () {
-        assert.equal(tlks.talk.popular(), true);
+    it('get the most popular talks', function (done) {
+        tlks.talk.popular(dburl, 5, function (err, docs) {
+            if (err) {
+                throw new Error(err);
+            }
+            assert.notEqual(docs, null);
+            done();
+        });
     });
 
-    it('create a new talk', function () {
-        assert.equal(tlks.talk.create(), true);
+    it('create a new talk', function (done) {
+        var talk = {
+            id: 'talk_id',
+            code: 'talk_code',
+            title: 'Talk title',
+            slug: 'talk-title',
+            description: 'Talk description',
+            author: {
+                id: 'author_authorid',
+                username: 'author_username',
+                avatar: 'http://example.com/avatar.jpg'
+            },
+            viewCount: 0,
+            voteCount: 0,
+            votes: [],
+            favoriteCount: 0,
+            favorites: [],
+            tags: [
+                'tag1',
+                'tag2',
+                'tag3'
+            ],
+            created: 1423881000025,
+            updated: 1423881000025
+        };
+        tlks.talk.createTalk(dburl, talk, function (err, docs) {
+            if (err) {
+                throw new Error(err);
+            }
+            assert.notEqual(docs, null);
+            done();
+        });
     });
 
     it('get a talk', function () {
