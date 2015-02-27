cb135a0f03c796c8cf0e38fcc9990fa7dc571ee4
Raül Pérez <repejota@gmail.com>
2015-02-27 04:31:16 +0100

    Added test coverage

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 test:
 	./node_modules/mocha/bin/mocha
 
+cover:
+	./node_modules/.bin/istanbul cover ./node_modules/.bin/_mocha -- -R spec
+
 lint:
 	./node_modules/.bin/gulp
 
--- index.js
+++ index.js
@@ -3,4 +3,3 @@ var packagejson = require('./package.json');
 exports.version = packagejson.version;
 
 exports.talk = require('./lib/talk.js');
-exports.search = require('./lib/search.js');
