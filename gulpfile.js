2f5d2e7db64c86068b0a4c3873aa656d43bf9652
Raül Pérez <repejota@gmail.com>
2015-03-07 03:04:39 +0100

    Generate dist files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,29 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('clean', function() {
     return gulp.src(['node_modules', 'coverage'], {
             read: false
         })
         .pipe(clean());
+});
+
+gulp.task('dist', function() {
+    return gulp.src(['./index.js', './lib/**/*.js'])
+        .pipe(sourcemaps.init())
+        .pipe(concat('libtlks.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('libtlks.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('jshint', function() {
