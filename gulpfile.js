072bb63a7f99e2a45474396c77b6d9fe54268e24
Raül Pérez <repejota@gmail.com>
2015-03-15 00:14:07 +0100

    typo

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
+var coveralls = require('gulp-coveralls');
 
 gulp.task('clean', function() {
     return gulp.src(['node_modules', 'coverage'], {
@@ -30,6 +31,11 @@ gulp.task('jshint', function() {
     return gulp.src(['./lib/**/*.js', 'index.js', 'gulpfile.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
+});
+
+gulp.task('coveralls', function() {
+    gulp.src('./coverage/**/lcov.info')
+        .pipe(coveralls());
 });
 
 gulp.task('default', ['jshint'], function() {});
